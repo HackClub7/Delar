5e3a95b35e3159712dc07cb832718e70a99e2ed3
Jethrolopwus <lopwusjethro92@gmail.com>
2024-10-06 02:48:30 +0100

    chores: Navbar and buyers page set up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,13 +8,14 @@ import {
 } from "react-router-dom";
 import Home from './components/Home'
 import Root from './Root'
+import BuyersPage from './pages/BuyersPage';
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root/>}>
         <Route index element={<Home/>}/>
-
+        <Route path='/buyerspage' element={<BuyersPage/>}/>
       </Route>
 
     )
