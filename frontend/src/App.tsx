59f3a836cb3b7d926bdb5797551becae7acfc904
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-10-08 14:47:13 +0100

    integrate view owner lands

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,8 @@ import BuyersPage from './pages/BuyersPage';
 import Register from './pages/Register';
 import MyLands from './pages/MyLands';
 import "../connection";
+import useContract from './hooks/useContract';
+import { useCallback, useEffect, useState } from 'react';
 // import "../connection";
 
 // import { createAppKit } from "@reown/appkit/react";
@@ -56,6 +58,25 @@ import "../connection";
 
 
 function App() {
+  const readOnlyDelarContract = useContract(true);
+  const [lands, setLands] = useState([]);
+
+  const fetchOwnerlands = useCallback(async () => {
+    if(!readOnlyDelarContract) return;
+
+    try {
+        const ownerLands = await readOnlyDelarContract.veiwOwnerLands("0xE859ac304020Dd3039082827d2Cbd25979297BDD")
+        const ownerLandData = { ...ownerLands };
+        console.log(ownerLandData);
+    } catch (error) {
+      console.log("error fetching owner lands:", error);
+    }
+  }, [readOnlyDelarContract]);
+
+  useEffect(() => {
+    fetchOwnerlands();
+  }, [fetchOwnerlands]);
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root/>}>
