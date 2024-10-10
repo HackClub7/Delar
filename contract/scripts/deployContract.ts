9d5d81ecb3f16d94a3415486269b5a0c9d6251a5
mananuf <bmananaf@gmail.com>
2024-10-10 11:57:44 +0100

    separate deploy scripts

--- contract/scripts/deployContract.ts
+++ contract/scripts/deployContract.ts
@@ -1,16 +1,10 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-//   const DelarToken = await ethers.deployContract('DelarToken');
-
-//   await DelarToken.waitForDeployment();
-
-//   console.log('DelarToken Contract Deployed at ' + DelarToken);
-
     const DelarContractFactory = await ethers.getContractFactory('DelarContract');
     
     // Deploy the contract
-    const DelarContract = await DelarContractFactory.deploy('0x49aC2AD1785d9577aF52a4Cd1511DcCC3AC42704');
+    const DelarContract = await DelarContractFactory.deploy('0x49aC2AD1785d9577aF52a4Cd1511DcCC3AC42704', '0x134Ae99f229340fAcbe6F68cd21235BAD97670CF');
 
     // Wait for the deployment to finish
     await DelarContract.waitForDeployment();
@@ -22,8 +16,6 @@ async function main() {
 
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
