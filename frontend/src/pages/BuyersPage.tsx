b5526ca2493c12d287f4c3fb73b46c3995ba9892
Jethrolopwus <lopwusjethro92@gmail.com>
2024-10-12 17:19:49 +0100

    fixes

--- frontend/src/pages/BuyersPage.tsx
+++ frontend/src/pages/BuyersPage.tsx
@@ -45,16 +45,9 @@ const BuyersPage = () => {
   return (
     <section className="container mx-auto overflow-x-hidden">
       <div className="min-h-screen mt-10 md:mt-0 flex flex-col items-center justify-center">
-
         <div className="w-full flex flex-row justify-between">
 
           <div className=" w-[65%] ml-8">
-
-        <div className="w-full flex flex-col  md:flex-row justify-between ">
-
-          <div className="w-[85%] md:w-[65%] ml-8">
-            {/* recently listed */}
-
             <div className="w-full h-[50%]">
               <p className="text-black mb-4 text-lg md:text-xl">Recently Listed</p>
 
@@ -125,17 +118,8 @@ const BuyersPage = () => {
 
           <div className="text-center mr-4">
             <p className="text-black mb-4 text-lg md:text-xl">Hottest Land</p>
-
             <div className="relative w-full max-w-xs md:max-w-sm h-[665px] mx-auto">
               <img className="w-full h-full object-cover rounded-3xl" src={landImage} alt="land" />
-
-            <div className="relative w-full max-w-xs md:max-w-sm h-[400px] md:h-[665px] mx-auto">
-              <img
-                className="w-full h-full object-cover rounded-3xl"
-                src={landImage}
-                alt="land"
-              />
-
               <div className="absolute inset-0 flex items-end justify-center p-4 bg-gradient-to-t from-black/60 to-transparent rounded-3xl">
                 <div className="w-full">
                   {lands[0] && (
