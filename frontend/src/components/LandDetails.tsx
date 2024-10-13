05f3a41c2071bf70e60f86451b8e930820fb3273
Jethrolopwus <lopwusjethro92@gmail.com>
2024-10-13 09:36:16 +0100

    fixes on the landdetails

--- frontend/src/components/LandDetails.tsx
+++ frontend/src/components/LandDetails.tsx
@@ -1,16 +1,19 @@
 import icon from "../assets/location.svg";
-import { Link } from "react-router-dom";
-
 
 interface LandDetailsProps {
   numberOfPlots: number;
   landLocation: string;
   titleNumber: string;
- plotForSale: string;
- netWorth: number;
+  plotForSale: string;
+  netWorth: number;
 }
 
-const LandDetails: React.FC<LandDetailsProps> = ({ numberOfPlots, landLocation, titleNumber, plotForSale }) => {
+const LandDetails: React.FC<LandDetailsProps> = ({
+  numberOfPlots,
+  landLocation,
+  titleNumber,
+  plotForSale,
+}) => {
   return (
     <div className="rounded-lg flex flex-col h-full px-2 mt-2">
       <div className="flex items-center text-black">
@@ -18,17 +21,15 @@ const LandDetails: React.FC<LandDetailsProps> = ({ numberOfPlots, landLocation, 
         <p className="text-xs md:text-sm truncate ml-3">{landLocation}</p>
       </div>
 
-      <p className="text-black text-xs md:text-sm">Available Plots: {numberOfPlots}</p>
-      <p className="text-black text-xs md:text-sm mt-1 font-bold">Land ID: {titleNumber}</p>
-      <p className="text-black text-xs md:text-sm mt-1 font-bold">forSale: {plotForSale}</p>
-
-      <div className="flex items-center  w-full text-black justify-end mb-3">
-        <Link to={"/land-details"}>
-          <button className="border border-black  rounded-2xl p-3 text-xs md:text-sm hover:bg-[#C3A46B] hover:text-black transition duration-300">
-            See Details
-          </button>
-        </Link>
-      </div>
+      <p className="text-black text-xs md:text-sm">
+        Available Plots: {numberOfPlots}
+      </p>
+      <p className="text-black text-xs md:text-sm mt-1 font-bold">
+        Title No: {titleNumber}
+      </p>
+      <p className="text-black text-xs md:text-sm mt-1 font-bold">
+        forSale: {plotForSale}
+      </p>
     </div>
   );
 };
